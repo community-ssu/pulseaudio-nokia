055ef295fcd93d97574a121cc6cc1a2f5005ae1d
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-11 23:13:45 +0300

    drc: mudrc_init: remove extra variable. While at it, fix the coding style.
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/drc/drc.c
+++ src/drc/drc.c
@@ -12,24 +12,21 @@ void
 mudrc_init(mumdrc_userdata_t *mumdrc, const int blockSize,
            const float sampleRate)
 {
-  EAP_MultibandDrcControl *control;
   EAP_MultibandDrcInt32_InitInfo initInfo;
 
-  control = &mumdrc->control;
   if (EAP_MultibandDrcControlInt32_Init(&mumdrc->control, sampleRate, 3, 1, 4.0,
                                         1.0, 1000.0, blockSize))
     pa_log_debug("DRC Control Init Failed");
 
-  EAP_MultibandDrcControlInt32_GetProcessingInitInfo(control, &initInfo);
+  EAP_MultibandDrcControlInt32_GetProcessingInitInfo(&mumdrc->control,
+                                                     &initInfo);
   mumdrc->DRCnMemRecs = EAP_MultibandDrcInt32_MemoryRecordCount(&initInfo);
 
   mumdrc->DRCpMemRecs = (EAP_MemoryRecord *)
       malloc(mumdrc->DRCnMemRecs * sizeof(EAP_MemoryRecord));
 
   if (!mumdrc->DRCpMemRecs)
-  {
     pa_log_debug("DRCpMemRecs Allocation Failed");
-  }
 
   EAP_MultibandDrcInt32_MemoryNeed(mumdrc->DRCpMemRecs, &initInfo);
 
