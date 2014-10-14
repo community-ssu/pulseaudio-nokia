71a6a42f87ba86362a1a0e3201d69b390ff322e0
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-14 23:14:11 +0300

    EAP: add EAP_WfirDummyInt32_Process
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_wfir_dummy_int32.c
+++ src/eap/eap_wfir_dummy_int32.c
@@ -1,8 +1,33 @@
+#include <string.h>
+
 #include "eap_wfir_dummy_int32.h"
 
 void
 EAP_WfirDummyInt32_Init(EAP_WfirInt32 *fir, int sampleRate)
 {
   fir->warpingShift = 0;
+}
+
+void
+EAP_WfirDummyInt32_Process(EAP_WfirInt32 *fir,
+                           int32 *const *leftChannel,
+                           int32 *const *rightChannel,
+                           int32 *unk1, int32 *unk2, const int32 *unk3,
+                           const int32 *unk4, const int32 *unk5,
+                           const int32 *unk6, int sampleCount)
+{
+  sampleCount *= sizeof(int32);
+
+  if (leftChannel[0] != unk3)
+    memcpy(leftChannel[0], unk3, sampleCount);
+
+  if ( *(const int32 **)rightChannel != unk4 )
+    memcpy(*rightChannel, unk4, sampleCount);
+
+  if ( unk5 != unk1 )
+    memcpy(unk1, unk5, sampleCount);
+
+  if ( unk6 != unk2 )
+    memcpy(unk2, unk6, sampleCount);
 }
 
--- src/eap/eap_wfir_dummy_int32.h
+++ src/eap/eap_wfir_dummy_int32.h
@@ -6,4 +6,12 @@
 void
 EAP_WfirDummyInt32_Init(EAP_WfirInt32 *fir, int sampleRate);
 
+void
+EAP_WfirDummyInt32_Process(EAP_WfirInt32 *fir,
+                           int32 *const *leftChannel,
+                           int32 *const *rightChannel,
+                           int32 *unk1, int32 *unk2, const int32 *unk3,
+                           const int32 *unk4, const int32 *unk5,
+                           const int32 *unk6, int sampleCount);
+
 #endif // EAP_WFIR_DUMMY_INT32_H
