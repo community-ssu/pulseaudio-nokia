35b5ff193c0d96ba1696ff5e17297c0bc81f9038
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-14 23:13:41 +0300

    EAP: eap_wfir_int32.h - add missing include. fix codeing style
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_wfir_int32.h
+++ src/eap/eap_wfir_int32.h
@@ -1,13 +1,18 @@
 #ifndef EAP_WFIR_INT32_H
 #define EAP_WFIR_INT32_H
 
+#include "eap_data_types.h"
+
 struct _EAP_WfirInt32
 {
   int warpingShift;
 };
 typedef struct _EAP_WfirInt32 EAP_WfirInt32;
 
-typedef void (*EAP_WfirInt32_ProcessFptr)(EAP_WfirInt32 *, int32 *const *, int32 *const *, int32 *, int32 *, const int32 *, const int32 *, const int32 *, const int32 *, int);
+typedef void (*EAP_WfirInt32_ProcessFptr)(EAP_WfirInt32 *, int32 *const *,
+                                          int32 *const *, int32 *, int32 *,
+                                          const int32 *, const int32 *,
+                                          const int32 *, const int32 *, int);
 typedef void (*EAP_WfirInt32_InitFptr)(EAP_WfirInt32 *, int);
 
 #endif // EAP_WFIR_INT32_H
