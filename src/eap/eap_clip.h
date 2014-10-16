7d1a69a7e2864a269e855253cdefce7dd8f01443
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-17 02:05:18 +0300

    EAP: add eap_clip.h
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

---
+++ src/eap/eap_clip.h
@@ -0,0 +1,18 @@
+#ifndef EAP_CLIP_H
+#define EAP_CLIP_H
+
+#include "eap_data_types.h"
+
+static int32
+EAP_Clip16(int32 input)
+{
+  if (input < EAP_INT16_MIN)
+    return EAP_INT16_MIN;
+
+  if (input > EAP_INT16_MAX)
+    return EAP_INT16_MAX;
+
+  return input;
+}
+
+#endif // EAP_CLIP_H
