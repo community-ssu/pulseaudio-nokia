4269ae80d36f4ee9b0c71300144e051b4c9b4ba8
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-19 15:41:19 +0300

    EAP: add EAP_WfirThreeBandsInt32_filter
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_wfir_three_bands_int32.c
+++ src/eap/eap_wfir_three_bands_int32.c
@@ -5,6 +5,7 @@
 #endif
 
 #include "eap_wfir_three_bands_int32.h"
+#include "eap_warped_delay_line_int32.h"
 
 struct _EAP_WfirThreeBandsInt32
 {
@@ -42,6 +43,92 @@ EAP_WfirThreeBandsInt32_Init(EAP_WfirInt32 *instance, int32 sampleRate)
   inst->m_rightCompMem[2] = 0;
   inst->m_rightCompMem[3] = 0;
 }
+
+#ifdef __ARM_NEON__
+static const int32x2_t h0_opt_1 = {0x20000000, 0x1B400000};
+static const int32x2_t h0_opt_2 = {0xFFB0000, 0x4C40000};
+static const int32x2_t h1_opt_1 = {0x40000000, 0x1FF60000};
+
+#define PROCESS_CHANNEL(ptr, out) \
+  W0 = vld1q_s32(&ptr[0]); \
+  W8 = vld1q_s32(&ptr[8]); \
+\
+  A = vaddq_s32(vqdmulhq_lane_s32 (W0, h0_opt_1, 0), \
+                vqdmulhq_lane_s32 (W8, h0_opt_2, 0)); \
+\
+  vst1q_s32(&out[1][i], \
+            vsubq_s32(vqdmulhq_lane_s32 (W0, h1_opt_1, 0), \
+                      vqdmulhq_lane_s32 (W8, h1_opt_1, 1))); \
+\
+  B = vaddq_s32(vqdmulhq_lane_s32 (vld1q_s32(&ptr[4]), h0_opt_1, 1), \
+                vqdmulhq_lane_s32 (vld1q_s32(&ptr[12]), h0_opt_2, 1)); \
+\
+  vst1q_s32(&out[0][i], vaddq_s32(A, B)); \
+  vst1q_s32(&out[2][i], vsubq_s32(A, B));
+
+#define PROCESS_CHANNEL_RESIDUE(ptr, out) \
+  W0 = vld1q_s32(&ptr[0]); \
+  W8 = vld1q_s32(&ptr[8]); \
+\
+  A = vaddq_s32(vqdmulhq_lane_s32 (W0, h0_opt_1, 0), \
+                vqdmulhq_lane_s32 (W8, h0_opt_2, 0)); \
+\
+  OUT1 = vsubq_s32(vqdmulhq_lane_s32 (W0, h1_opt_1, 0), \
+                      vqdmulhq_lane_s32 (W8, h1_opt_1, 1)); \
+\
+  B = vaddq_s32(vqdmulhq_lane_s32 (vld1q_s32(&ptr[4]), h0_opt_1, 1), \
+                vqdmulhq_lane_s32 (vld1q_s32(&ptr[12]), h0_opt_2, 1)); \
+\
+  OUT0 = vaddq_s32(A, B); \
+  OUT2 = vsubq_s32(A, B); \
+\
+  out[0][i] = vgetq_lane_s32(OUT0, 0); \
+  out[1][i] = vgetq_lane_s32(OUT1, 0); \
+  out[2][i] = vgetq_lane_s32(OUT2, 0); \
+\
+  if(residue == 2) \
+  { \
+    out[0][i + 1] = vgetq_lane_s32(OUT0, 1); \
+    out[1][i + 1] = vgetq_lane_s32(OUT1, 1); \
+    out[2][i + 1] = vgetq_lane_s32(OUT2, 1); \
+  } \
+\
+  if(residue == 3) \
+  { \
+    out[0][i + 1] = vgetq_lane_s32(OUT0, 1); \
+    out[1][i + 1] = vgetq_lane_s32(OUT1, 1); \
+    out[2][i + 1] = vgetq_lane_s32(OUT2, 1); \
+    out[0][i + 2] = vgetq_lane_s32(OUT0, 2); \
+    out[1][i + 2] = vgetq_lane_s32(OUT1, 2); \
+    out[2][i + 2] = vgetq_lane_s32(OUT2, 2); \
+  }
+
+void
+EAP_WfirThreeBandsInt32_filter(int32 *w_left, int32 *w_right,
+                               int32 *const *leftLowOutputBuffers,
+                               int32 *const *rightLowOutputBuffers,
+                               int sample_count)
+{
+  int residue = sample_count & 3;
+  int i;
+  int32x4_t W0, W8, A, B;
+
+  for (i = 0; i < sample_count - residue; i += 4, w_left += 16, w_right += 16)
+  {
+    PROCESS_CHANNEL(w_left, leftLowOutputBuffers);
+    PROCESS_CHANNEL(w_right, rightLowOutputBuffers);
+  }
+
+  if (residue)
+  {
+    int32x4_t OUT0, OUT1, OUT2;
+    PROCESS_CHANNEL_RESIDUE(w_left, leftLowOutputBuffers);
+    PROCESS_CHANNEL_RESIDUE(w_right, rightLowOutputBuffers);
+  }
+}
+#else
+#error TODO - x86 implementation. You need __ARM_NEON__ defined :)
+#endif
 
 void
 EAP_WfirThreeBandsInt32_Process(EAP_WfirInt32 *instance,
@@ -98,7 +185,8 @@ EAP_WfirThreeBandsInt32_Process(EAP_WfirInt32 *instance,
   EAP_WfirThreeBandsInt32_filter(inst->common.w_left,
                                  inst->common.w_right,
                                  leftLowOutputBuffers,
-                                 rightLowOutputBuffers);
+                                 rightLowOutputBuffers,
+                                 frames);
 
   for (i = 0; i < frames; i ++)
   {
--- src/eap/eap_wfir_three_bands_int32.h
+++ src/eap/eap_wfir_three_bands_int32.h
@@ -18,4 +18,10 @@ EAP_WfirThreeBandsInt32_Process(EAP_WfirInt32 *instance,
                                 const int32 *rightHighInput,
                                 int frames);
 
+void
+EAP_WfirThreeBandsInt32_filter(int32 *w_left, int32 *w_right,
+                               int32 *const *leftLowOutputBuffers,
+                               int32 *const *rightLowOutputBuffers,
+                               int sample_count);
+
 #endif // EAP_WFIR_THREE_BANDS_INT32_H
