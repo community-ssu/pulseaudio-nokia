953115682d2b297ea4332e178ff94af7f19c9960
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-24 01:06:13 +0300

    EAP: add EAP_WfirFiveBandsInt32_filter
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_wfir_five_bands_int32.h
+++ src/eap/eap_wfir_five_bands_int32.h
@@ -3,6 +3,10 @@
 
 #include "eap_wfir_int32.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void
 EAP_WfirFiveBandsInt32_Init(EAP_WfirInt32 *instance, int32 sampleRate);
 
@@ -17,5 +21,15 @@ EAP_WfirFiveBandsInt32_Process(EAP_WfirInt32 *instance,
                                const int32 *leftHighInput,
                                const int32 *rightHighInput,
                                int frames);
+
+void
+EAP_WfirFiveBandsInt32_filter(int32 *w_left, int32 *w_right,
+                              int32 *const *leftLowOutputBuffers,
+                              int32 *const *rightLowOutputBuffers,
+                              int sample_count);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // EAP_WFIR_FIVE_BANDS_INT32_H
