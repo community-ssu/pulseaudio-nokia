6e54fa92e8445a16da4dfb35f6804f0e478751e8
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-10-25 10:41:47 +0300

    add empty eap_amplitude_to_gain_int32 .c and .h
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_amplitude_to_gain_int32.h
+++ src/eap/eap_amplitude_to_gain_int32.h
@@ -1,4 +1,10 @@
+#ifndef EAP_AMPLITUDE_TO_GAIN_INT32_H
+#define EAP_AMPLITUDE_TO_GAIN_INT32_H
+
 #include "eap_multiband_drc_int32.h"
 
 void
 EAP_AmplitudeToGainInt32(const EAP_CompressionCurveImplDataInt32 *curveData, int32 *gainOutput, const int32 *amplitudeInput, int frames);
+
+
+#endif // EAP_AMPLITUDE_TO_GAIN_INT32_H
