0e526cd9abd5261793c6b210d2e7c40f276c433c
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2014-11-02 22:22:18 +0200

    EAP: fix EAP_AmplitudeToGainInt32
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/eap/eap_amplitude_to_gain_int32.c
+++ src/eap/eap_amplitude_to_gain_int32.c
@@ -14,15 +14,8 @@ EAP_AmplitudeToGainInt32(const EAP_CompressionCurveImplDataInt32 *curveData,
   {
     int i;
     int32 K;
-    int gainMant;
-    unsigned int arg;
-    int16 fracPol;
     int16 AExp;
     int16 AFrac;
-    int16 gainExp;
-    int16 fracArg;
-    int16 fracArg035;
-    int16 fracArg065;
     int32 ampl = amplitudeInput[f];
 
     for (i = 0; i <= 4 && curveData->levelLimits[i] < ampl; i ++);
@@ -33,39 +26,42 @@ EAP_AmplitudeToGainInt32(const EAP_CompressionCurveImplDataInt32 *curveData,
 
     if (K)
     {
-      int16 intArg;
-      int16 amplExp = EAP_Norm32(ampl);
-      int16 frac16 = (ampl << amplExp >> 15) + -32768;
-      int16 frac035 = 11469 * (int16)((ampl << amplExp >> 15) + -32768) >> 15;
+      int32 gainMant;
+      int32 arg;
+      int16 fracPol;
+      int16 gainExp;
+      int16 fracArg;
+      int16 fracArg035;
+      int16 fracArg065;
       int32 absArg;
-      int32 log2Ampl = ((7 - amplExp) << 15) + frac16 + 11469 * (frac16 >> 15) -
-          (frac16 * (frac035) >> 15);
+      int16 intArg;
+      int16 amplExp = __builtin_clz(abs(ampl)) - 1;
+      int16 frac16 = (ampl << amplExp >> 15) + EAP_INT16_MIN;
+      int16 frac035 = 11469 * (frac16) >> 15;
+      int32 log2Ampl =
+          ((7 - amplExp) << 15) + frac16 + frac035 - (frac16 * frac035 >> 15);
 
       arg = EAP_LongMult32Q15x32(log2Ampl, K);
 
-      if (arg >> 31 )
-        absArg = -arg;
-      else
-        absArg = arg;
-
+      absArg = abs(arg);
       intArg = absArg >> 15;
-      fracArg = absArg & 0x7FFF;
+      fracArg = absArg & EAP_INT16_MAX;
       fracArg035 = 11469 * fracArg >> 15;
       fracArg065 = 21299 * fracArg >> 15;
-      fracPol = fracArg035 * intArg + fracArg065;
+      fracPol = (fracArg035 * fracArg >> 15) + fracArg065;
 
-      if (arg >> 31)
+      if (arg < 0)
         Inverse(&fracPol, &intArg);
 
       gainExp = intArg + AExp;
-      gainMant = fracPol + 32768 + AFrac + (fracPol * AFrac >> 15);
+      gainMant = fracPol - EAP_INT16_MIN + AFrac + (fracPol * AFrac >> 15);
 
-      gainOutput[f] = (((int16)(intArg + AExp) <= 0) ? gainMant >> -gainExp :
-                                                       gainMant << gainExp);
+      gainOutput[f] =
+          ((intArg + AExp <= 0) ? gainMant >> -gainExp : gainMant << gainExp);
     }
     else
     {
-      int result = AFrac + 32768;
+      int result = AFrac - EAP_INT16_MIN;
       gainOutput[f] = ((AExp <= 0) ? (result >> -AExp) : (result << AExp));
     }
   }
