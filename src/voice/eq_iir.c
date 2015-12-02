e9bec39293e0e3e9972019318d1a71a76af3d575
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2015-12-02 15:24:36 +0200

    add iir_eq_change_params
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/voice/eq_iir.c
+++ src/voice/eq_iir.c
@@ -17,70 +17,93 @@
 #include <dbus/dbus.h>
 #include "eq_iir.h"
 
-void iir_eq_process_mono(struct iir_eq *eq, int16_t *src, int16_t *dst, int32_t samples)
+void
+iir_eq_process_mono(struct iir_eq *eq, int16_t *src, int16_t *dst,
+                    int32_t samples)
 {
   //todo address 0x0002365C
 }
 
-void iir_eq_free(struct iir_eq *eq)
+void
+iir_eq_free(struct iir_eq *eq)
 {
   if (eq->channel.coeff[1])
-  {
     free(eq->channel.coeff[1]);
-  }
+
   if (eq->channel.delay[1])
-  {
     free(eq->channel.delay[1]);
-  }
+
   free(eq->channel.coeff[0]);
   free(eq->channel.delay[0]);
   free(eq->scratch);
   free(eq);
 }
 
-struct iir_eq *iir_eq_new(int max_samples_per_frame, int channels)
+struct iir_eq *
+    iir_eq_new(int max_samples_per_frame, int channels)
 {
-  struct iir_eq *i = pa_xnew(struct iir_eq, 1);
-  if (!i)
-  {
+  struct iir_eq *eq = pa_xnew(struct iir_eq, 1);
+
+  if (!eq)
     return NULL;
-  }
-  i->scratch = (int32_t *)pa_xmalloc(channels * max_samples_per_frame * 4);
-  if (i->scratch)
+
+  eq->scratch = (int32_t *)pa_xmalloc(channels * max_samples_per_frame * 4);
+
+  if (eq->scratch)
   {
-    i->channel.delay[0] = (int32_t *)pa_xmalloc0(40);
-    if (i->channel.delay[0])
+    eq->channel.delay[0] = (int32_t *)pa_xmalloc0(40);
+
+    if (eq->channel.delay[0])
     {
-      i->channel.coeff[0] = (int16_t *)pa_xmalloc(76);
-      if (i->channel.coeff[0])
+      eq->channel.coeff[0] = (int16_t *)pa_xmalloc(76);
+
+      if (eq->channel.coeff[0])
       {
         if (channels != 2)
         {
-          i->channel.coeff[1] = 0;
-          i->channel.delay[1] = 0;
-          return i;
+          eq->channel.coeff[1] = NULL;
+          eqi->channel.delay[1] = NULL;
+
+          return eq;
         }
-        i->channel.delay[1] = (int32_t *)pa_xmalloc0(40);
-        if (i->channel.delay[1])
+
+        eq->channel.delay[1] = (int32_t *)pa_xmalloc0(40);
+
+        if (eq->channel.delay[1])
         {
-          i->channel.coeff[1] = (int16_t *)pa_xmalloc(76);
-          if (i->channel.coeff[1])
-          {
-            return i;
-          }
-          free(i->channel.delay[1]);
+          eq->channel.coeff[1] = (int16_t *)pa_xmalloc(76);
+
+          if (eq->channel.coeff[1])
+            return eq;
+
+          free(eq->channel.delay[1]);
         }
-        free(i->channel.coeff[0]);
+
+        free(eq->channel.coeff[0]);
       }
-      free(i->channel.delay[0]);
+
+      free(eq->channel.delay[0]);
     }
-    free(i->scratch);
+
+    free(eq->scratch);
   }
-  free(i);
-  return 0;
+
+  free(eq);
+
+  return NULL;
 }
 
-void iir_eq_change_params(struct iir_eq *eq, const void *parameters, size_t length)
+void
+iir_eq_change_params(struct iir_eq *eq, const void *parameters, size_t length)
 {
-  //todo address 0x000237CC
+  int count = parameters->count + 1;
+
+  memcpy(eq->channel.coeff[0], parameters, 14 * count + 6);
+  memset(eq->channel.delay[0], 0, 8 * count);
+
+  if (eq->channel.coeff[1])
+    memcpy(eq->channel.coeff[1], parameters, 14 * count + 6);
+
+  if (eq->channel.delay[1])
+    memset(eq->channel.delay[1], 0, 8 * count);
 }
--- src/voice/eq_iir.h
+++ src/voice/eq_iir.h
@@ -1,3 +1,6 @@
+#ifndef __EQ_IIR_H__
+#define __EQ_IIR_H__
+
 struct eq_channel
 {
   int16_t *coeff[2];
@@ -10,7 +13,11 @@ struct iir_eq
   int32_t *scratch;
 };
 
-void iir_eq_process_mono(struct iir_eq *eq, int16_t *src, int16_t *dst, int32_t samples);
+void iir_eq_process_mono(struct iir_eq *eq, int16_t *src, int16_t *dst,
+                         int32_t samples);
 void iir_eq_free(struct iir_eq *eq);
 struct iir_eq *iir_eq_new(int max_samples_per_frame, int channels);
-void iir_eq_change_params(struct iir_eq *eq, const void *parameters, size_t length);
+void iir_eq_change_params(struct iir_eq *eq, const void *parameters,
+                          size_t length);
+
+#endif
